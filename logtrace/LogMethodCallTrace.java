f42e209d6b1921b58b787a41e3a5731bc2f49aab
zenyo <lzy@lcs.iscas>
2012-07-25 22:29:52 +0800

    Try to figure out the call and return of adding and removing in Concurrent Queue.

--- logtrace/LogMethodCallTrace.java
+++ logtrace/LogMethodCallTrace.java
@@ -1,5 +1,6 @@
 package logtrace;
 
+import com.sun.btrace.AnyType;
 import com.sun.btrace.BTraceUtils;
 import com.sun.btrace.BTraceUtils.*;
 import com.sun.btrace.annotations.*;
@@ -9,8 +10,8 @@ public class LogMethodCallTrace {
 	//@TLS static Appendable c = Strings.newStringBuilder(true);
 	//@TLS static Appendable r = Strings.newStringBuilder(true);
 	
-	@OnMethod (clazz="target.HelloWorld", method="/func.+/", location=@Location(value=Kind.ENTRY))
-	public static void recordFuncEnter (@ProbeClassName String className, @ProbeMethodName String methodName) {
+	@OnMethod (clazz="java.util.concurrent.ConcurrentLinkedQueue", method="/add|poll/", location=@Location(value=Kind.ENTRY))
+    public static void recordFuncEnter (@ProbeClassName String className, @ProbeMethodName String methodName, AnyType[] objs) {
 		Appendable c = Strings.newStringBuilder(true);
 		Strings.append(c, BTraceUtils.str(BTraceUtils.threadId(BTraceUtils.currentThread())));
 		Strings.append(c, " call ");
@@ -19,11 +20,16 @@ public class LogMethodCallTrace {
 		Strings.append(c, methodName);
 		Strings.append(c, " ");
 		Strings.append(c, Strings.str(BTraceUtils.timeMillis()));
+		Strings.append(c, " - ");
+		Strings.append(c, BTraceUtils.printArrayToString(objs));				
+		
+		// Print
 		BTraceUtils.println(Strings.str(c));
 	}
 	
-	@OnMethod (clazz="target.HelloWorld", method="/func.+/", location=@Location(value=Kind.RETURN))
-	public static void recordFuncExit (@ProbeClassName String className, @ProbeMethodName String methodName) {
+	@OnMethod (clazz="java.util.concurrent.ConcurrentLinkedQueue", method="add", location=@Location(value=Kind.RETURN))
+	public static void recordAddReturn (@ProbeClassName String className, 
+			@ProbeMethodName String methodName, @Return boolean returnVal) {
 		Appendable r = Strings.newStringBuilder(true);
 		Strings.append(r, BTraceUtils.str(BTraceUtils.threadId(BTraceUtils.currentThread())));
 		Strings.append(r, " return ");
@@ -32,6 +38,51 @@ public class LogMethodCallTrace {
 		Strings.append(r, methodName);
 		Strings.append(r, " ");
 		Strings.append(r, Strings.str(BTraceUtils.timeMillis()));
+		Strings.append(r, " - ");	
+		Strings.append(r, Strings.str(returnVal));
+
+		//Print
 		BTraceUtils.println(Strings.str(r));
 	}
+	
+	@OnMethod (clazz="java.util.concurrent.ConcurrentLinkedQueue", method="poll", location=@Location(value=Kind.RETURN))
+	public static void recordPollReturn (@ProbeClassName String className, 
+			@ProbeMethodName String methodName, @Return Object returnVal) {
+		Appendable r = Strings.newStringBuilder(true);
+		Strings.append(r, BTraceUtils.str(BTraceUtils.threadId(BTraceUtils.currentThread())));
+		Strings.append(r, " return ");
+		Strings.append(r, className);
+		Strings.append(r, " ");
+		Strings.append(r, methodName);
+		Strings.append(r, " ");
+		Strings.append(r, Strings.str(BTraceUtils.timeMillis()));
+		Strings.append(r, " - ");
+		if (returnVal == null) {
+			Strings.append(r, "null");
+		} else {
+			Strings.append(r, Strings.str(returnVal));
+		}
+
+		//Print
+		BTraceUtils.println(Strings.str(r));
+	}
+/*	@OnMethod (clazz="target.HelloWorld", method="/func.+/", location=@Location(value=Kind.RETURN))
+	public static void recordFuncExit (@ProbeClassName String className, 
+			@ProbeMethodName String methodName, @Return Object returnVal) {
+		Appendable r = Strings.newStringBuilder(true);
+		Strings.append(r, BTraceUtils.str(BTraceUtils.threadId(BTraceUtils.currentThread())));
+		Strings.append(r, " return ");
+		Strings.append(r, className);
+		Strings.append(r, " ");
+		Strings.append(r, methodName);
+		Strings.append(r, " ");
+		Strings.append(r, Strings.str(BTraceUtils.timeMillis()));
+		//if (returnVal != null) { 
+			Strings.append(r, " - ");	
+			Strings.append(r, Strings.str(returnVal));
+		//}
+		
+		//Print
+		BTraceUtils.println(Strings.str(r));
+	}*/
 }
